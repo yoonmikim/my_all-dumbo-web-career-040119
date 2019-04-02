c18490f92f0a02b3d2d0e61ce0691f1b86bf914c
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-02 13:03:18 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,9 +8,9 @@ def my_all?(collection)
     i = i + 1
   end
  
-  if block_return_values.include?(false)
-    false
-  else
+  if block_return_values.include?(true)
     true
+  else
+    false
   end
 end
